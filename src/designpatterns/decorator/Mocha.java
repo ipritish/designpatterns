a4021dd1e2e07d5e4f46ad5c798f59008fce48a7
ipritish <pritishjain2009@gmail.com>
2016-10-05 14:35:18 +0530

    decorator pattern

---
+++ src/designpatterns/decorator/Mocha.java
@@ -0,0 +1,21 @@
+package designpatterns.decorator;
+
+public class Mocha extends CondimentDecorator{
+	
+	Beverage beverage;
+	
+	public Mocha(Beverage b){
+		this.beverage = b;
+	}
+
+	@Override
+	public String getDescription() {
+		return beverage.getDescription() + ", Mocha";
+	}
+
+	@Override
+	public double cost() {
+		return beverage.cost() + 0.20;
+	}
+
+}
---
+++ src/designpatterns/decorator/Whip.java
@@ -0,0 +1,21 @@
+package designpatterns.decorator;
+
+public class Whip extends CondimentDecorator{
+	
+	Beverage beverage;
+	
+	public Whip(Beverage b){
+		this.beverage = b;
+	}
+
+	@Override
+	public String getDescription() {
+		return beverage.getDescription() + ", Whip";
+	}
+
+	@Override
+	public double cost() {
+		return beverage.cost() + 0.15;
+	}
+
+}
