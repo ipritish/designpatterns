ffb49d876223de09f48d2b81d4823e193b5ccdf0
ipritish <pritishjain2009@gmail.com>
2016-10-17 13:42:27 +0530

    test

--- src/designpatterns/observer/custom/WeatherStation.java
+++ src/designpatterns/observer/custom/WeatherStation.java
@@ -5,7 +5,7 @@ public class WeatherStation {
 	public static void main(String[] args){
 		WeatherData weatherData = new WeatherData();
 		
-		CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay(weatherData);
+		new CurrentConditionDisplay(weatherData);
 		
 		weatherData.setMeasurements(80, 65, 30.4f);
 		weatherData.setMeasurements(82, 70, 29.2f);
